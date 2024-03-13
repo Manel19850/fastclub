c931e65483764fe63d0cdf034379a8c7c364149b
manel <manel.jouinipro@outlook.com>
2024-03-13 13:33:22 +0100

    Fast Club 2.0

---
+++ src/component/Accueil.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useForm } from '@formspree/react';
+import './Accueil.css';
+
+const Accueil = () => {
+  const [state, handleSubmit] = useForm("xpzvlbgj"); // Remplacez "yourFormspreeId" par votre ID Formspree réel
+  const lienDePaiement = "https://www.linkedin.com/in/manel-jouini/"; // Remplacez ceci par l'URL réelle de votre page de paiement
+
+  // Redirige vers le lien de paiement après la soumission du formulaire
+  if (state.succeeded) {
+    window.location.href = lienDePaiement;
+  }
+
+  return (
+    <div className="accueil">
+      <h1 className="title">FAST CLUB 2.0</h1>
+      <div className="centered-content">
+        <h2>Prêt pour l'aventure Suisse?</h2>
+        <h3>N'attends plus!</h3>
+        <p>Abonne toi!</p>
+        <form onSubmit={handleSubmit} className="contact-form">
+          <input
+            type="text"
+            name="nom"
+            placeholder="Nom"
+            required
+          />
+          <input
+            type="text"
+            name="prenom"
+            placeholder="Prénom"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Adresse mail"
+            required
+          />
+          <button type="submit" disabled={state.submitting}>
+            Envoyer
+          </button>
+        </form>
+      </div>
+      <div className="logo-container">
+        <img src='LOGO.png' alt="Fast Club Logo" className="logo" />
+      </div>
+      <div className="circle circle-one"></div>
+      <div className="circle circle-two"></div>
+    </div>
+  );
+};
+
+export default Accueil;
